cec0dfac56b393e09f5b9f6ed393d23a6f9217b1
KarlSavinskiy <karlsavinskiy@gmail.com>
2022-07-25 16:36:37 -0400

    commit tree 566edf256d48c5e2d0c61a233400523b60956af6

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
 // Add your code here
-const formData = {
-    dogName: "Byron",
-    dogBreed: "Poodle",
-  };
-  
-    
-const configurationObject = {
-    method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-        },
-        body: JSON.stringify(formData)/* Your data goes here */
-      };
-fetch("http://localhost:3000/dogs", configurationObject) 
-.then(function (response) {
-    return response.json();
-  })
-  .then(function (object) {
-    console.log(object);
-  })
-  .catch(function (error) {
-    alert("Bad things! Ragnarők!");
-    console.log(error.message);
-  });
 
-  const submitDAta = () => {
-    
+  function submitData(name, email) {
+
+
+    return fetch('http://localhost:3000/users',{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body:JSON.stringify({
+      name,
+      email
+    })
+  })
+  .then(res => res.json())
+  .then(data => {document.body.innerHTML = data['id']})
+  .catch(err => document.body.innerHTML = err.message)
   }
