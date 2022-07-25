bf933b7f461d24eb82f143f26143813a7dcd5882
Karl Savinskiy <karlsavinskiy@gmail.com>
2022-07-25 04:31:43 -0700

    I'm doing this

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle",
+  };
+  
+    
+const configurationObject = {
+    method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formData)/* Your data goes here */
+      };
+fetch("http://localhost:3000/dogs", configurationObject) 
+.then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    console.log(object);
+  })
+  .catch(function (error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+  });
+
+  const submitDAta = () => {
+    
+  }
